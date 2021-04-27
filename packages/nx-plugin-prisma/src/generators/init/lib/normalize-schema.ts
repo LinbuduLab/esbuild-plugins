1d41cf79a94d171660f12de3122a996919997c1a
linbudu599 <linbudu599@gmail.com>
2021-04-27 20:00:45 +0800

    :sparkles: feat(nx-prisma): init generator schema

---
+++ packages/nx-plugin-prisma/src/generators/init/lib/normalize-schema.ts
@@ -0,0 +1,7 @@
+import { Tree, joinPathFragments, getWorkspaceLayout } from '@nrwl/devkit';
+import { normalizeNodeAppSchema } from 'nx-plugin-devkit';
+
+import {
+  PrismaInitGeneratorSchema,
+  NormalizedPrismaInitGeneratorSchema,
+} from '../schema';
--- packages/nx-plugin-prisma/src/generators/init/schema.d.ts
+++ packages/nx-plugin-prisma/src/generators/init/schema.d.ts
@@ -1,5 +1,27 @@
-export interface InitGeneratorSchema {
-  name: string;
-  tags?: string;
-  directory?: string;
+// init是只能对存在的app用吧？
+
+export interface PrismaInitGeneratorSchema {
+  app: string;
+
+  // prisma/schema.prisma
+  directory: string;
+  schemaName: string;
+  datasourceProvider: 'sqlite' | 'postgresql' | 'mysql' | 'sqlserver';
+  datasourceUrl: string;
+  // TODO: support file path
+  clientProvider: 'prisma-client-js';
+  watch: boolean;
+  createEnvFile: boolean;
+  // TODO:
+  // binaryTargets
+  // previewFeatures
+}
+
+export interface NormalizedPrismaInitGeneratorSchema
+  extends PrismaInitGeneratorSchema {
+  projectName: string;
+  projectRoot: string;
+  projectSourceRoot: string;
+  projectDirectory: string;
+  offsetFromRoot: string;
 }
