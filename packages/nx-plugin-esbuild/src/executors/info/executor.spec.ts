93605df75b2818e4e11eb7f09b5e693374d69ddc
linbudu599 <linbudu599@gmail.com>
2021-05-01 14:12:53 +0800

    :sparkles: feat(workspace): create info executor for all plugins

---
+++ packages/nx-plugin-esbuild/src/executors/info/executor.spec.ts
@@ -0,0 +1,11 @@
+import { InfoExecutorSchema } from './schema';
+import executor from './executor';
+
+const options: InfoExecutorSchema = {};
+
+describe('Info Executor', () => {
+  it('can run', async () => {
+    const output = await executor(options);
+    expect(output.success).toBe(true);
+  });
+});
