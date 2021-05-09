83b4785f12b54b8caf18cfbec469fdd12645ffaa
linbudu599 <linbudu599@gmail.com>
2021-05-09 16:23:56 +0800

    :sparkles: feat(nx-umi): initial build executor implemention

--- packages/nx-plugin-umi/src/executors/build/build.impl.ts
+++ packages/nx-plugin-umi/src/executors/build/build.impl.ts
@@ -1,5 +1,5 @@
 import { BuildExecutorSchema } from './schema';
-import { join } from 'path';
+import { join, resolve } from 'path';
 import { chalk, yParser } from '@umijs/utils';
 import { existsSync } from 'fs';
 import { Service } from 'umi/lib/ServiceWithBuiltIn';
@@ -8,20 +8,33 @@ import getCwd from 'umi/lib/utils/getCwd';
 import getPkg from 'umi/lib/utils/getPkg';
 import initWebpack from 'umi/lib/initWebpack';
 
-export default function runExecutor(options: BuildExecutorSchema) {
+export default async function runExecutor(options: Record<string, string>) {
   // 有无现成的ChildProcess转Promise？
-  const child = fork({
-    scriptPath: require.resolve('umi/lib/forkedDev'),
+  // const child = fork({
+  //   scriptPath: require.resolve('umi/lib/forkedDev'),
+  // });
+  // process.on('SIGINT', () => {
+  //   child.kill('SIGINT');
+  //   process.exit(0);
+  // });
+  // process.on('SIGTERM', () => {
+  //   child.kill('SIGTERM');
+  //   process.exit(1);
+  // });
+  // return {
+  //   success: true,
+  // };
+
+  process.env.NODE_ENV = 'production';
+  initWebpack(options.cwd);
+
+  await new Service({
+    // cwd: options.cwd,
+    // pkg: require(join(options.cwd, 'package.json')),
+    cwd: getCwd(),
+    pkg: getPkg(process.cwd()),
+  }).run({
+    name: 'build',
+    args: {},
   });
-  process.on('SIGINT', () => {
-    child.kill('SIGINT');
-    process.exit(0);
-  });
-  process.on('SIGTERM', () => {
-    child.kill('SIGTERM');
-    process.exit(1);
-  });
-  return {
-    success: true,
-  };
 }
