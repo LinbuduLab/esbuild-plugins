dc0eae7d2e3eb26cd15e8a28adbb8f204e20ba29
linbudu599 <linbudu599@gmail.com>
2021-05-09 15:52:56 +0800

    :sparkles: feat(nx-umi): umi serve executor updated

--- packages/nx-plugin-umi/src/executors/serve/serve.impl.ts
+++ packages/nx-plugin-umi/src/executors/serve/serve.impl.ts
@@ -10,33 +10,14 @@ import execa from 'execa';
 import { from } from 'rxjs';
 import { map, switchMap, tap } from 'rxjs/operators';
 import { eachValueFrom } from 'rxjs-for-await';
+import { startUmiServe } from '../utils/start-umi-serve';
 
 export default function runExecutor(options: any) {
   // 有无现成的ChildProcess转Promise？
-  // const child = fork({
-  //   scriptPath: require.resolve('umi/lib/forkedDev'),
-  // });
+  // 看起来不需要，只要模仿umi fork处理一下inspection相关即可
 
-  // const cp = execa.node(require.resolve('umi/lib/forkedDev'), {
-  //   cwd: options.cwd,
-  //   stdio: 'inherit',
-  // });
-
-  // process.on('SIGINT', () => {
-  //   child.kill('SIGINT');
-  //   process.exit(0);
-  // });
-  // process.on('SIGTERM', () => {
-  //   child.kill('SIGTERM');
-  //   process.exit(1);
-  // });
   return eachValueFrom(
-    from(
-      execa.node(require.resolve('umi/lib/forkedDev'), {
-        cwd: options.cwd,
-        stdio: 'inherit',
-      })
-    ).pipe(
+    startUmiServe(options.cwd).pipe(
       tap((x) => {
         console.log(x);
       }),
