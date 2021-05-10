aa96a54812c9e6da9dede0e6cfb997cbca5459e5
linbudu599 <linbudu599@gmail.com>
2021-05-10 13:08:44 +0800

    :sparkles: feat(nx-umi): update serve executor

--- packages/nx-plugin-umi/src/executors/serve/serve.impl.ts
+++ packages/nx-plugin-umi/src/executors/serve/serve.impl.ts
@@ -1,25 +1,11 @@
-import { join } from 'path';
-import { chalk, yParser } from '@umijs/utils';
-import { existsSync } from 'fs';
-import { Service } from 'umi/lib/ServiceWithBuiltIn';
-import fork from 'umi/lib/utils/fork';
-import getCwd from 'umi/lib/utils/getCwd';
-import getPkg from 'umi/lib/utils/getPkg';
-import initWebpack from 'umi/lib/initWebpack';
-import execa from 'execa';
-import { from } from 'rxjs';
 import { map, switchMap, tap } from 'rxjs/operators';
 import { eachValueFrom } from 'rxjs-for-await';
 import { startUmiServe } from '../utils/start-umi-serve';
 
 export default function runExecutor(options: any) {
-  // 有无现成的ChildProcess转Promise？
-  // 看起来不需要，只要模仿umi fork处理一下inspection相关即可
   return eachValueFrom(
     startUmiServe(options.cwd).pipe(
-      tap((x) => {
-        console.log(x);
-      }),
+      tap((x) => {}),
       map(() => {
         return {
           success: true,
--- packages/nx-plugin-umi/src/executors/utils/start-umi-serve.ts
+++ packages/nx-plugin-umi/src/executors/utils/start-umi-serve.ts
@@ -1,13 +1,32 @@
 import execa from 'execa';
 import { from } from 'rxjs';
+import path from 'path';
+import chalk from 'chalk';
 
 // TODO: umi fork inspect utils
 // TODO: execa options
+// dev命令不需要使用修改的initWebpack
 export const startUmiServe = (cwd: string) => {
-  const forkedDevProcess = execa.node(require.resolve('umi/lib/forkedDev'), {
-    cwd,
-    stdio: 'inherit',
-  });
+  const forkedDevProcess = execa.node(
+    require.resolve('umi/lib/forkedDev'),
+    // 使用nx schema 来进行支持
+    // ['-r', 'source-map-support/register', '--inspect-brk'],
+    {
+      cwd,
+      stdio: 'inherit',
+    }
+  );
+
+  const cwdPath = path.resolve(cwd);
+  console.log(chalk.green(`Using cwd path: ${cwdPath}`));
+
+  console.log(
+    chalk.green(
+      `Loading package.json from: ${path.resolve(cwdPath, 'package.json')}`
+    )
+  );
+
+  process.env.APP_ROOT = path.resolve(cwdPath, 'src');
 
   process.on('SIGINT', () => {
     forkedDevProcess.kill('SIGINT');
@@ -21,6 +40,12 @@ export const startUmiServe = (cwd: string) => {
 
   forkedDevProcess.on('message', (msg) => {
     console.log('msg: ', msg);
+    // 难道环境变量还会被重写
+    // console.log(
+    //   chalk.cyan(`process.env.UMI_VERSION: ${process.env.UMI_VERSION}`)
+    // );
+
+    // console.log(chalk.cyan(`process.env.UMI_DIR: ${process.env.UMI_DIR}`));
   });
 
   return from(forkedDevProcess);
