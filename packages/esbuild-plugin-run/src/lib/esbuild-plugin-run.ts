9ec4d126769083a497c427aa6d1fea19bab03f06
linbudu599 <linbudu599@gmail.com>
2021-05-13 11:40:38 +0800

    :sparkles: feat(esbuild-run): enable restart & clear

--- packages/esbuild-plugin-run/src/lib/esbuild-plugin-run.ts
+++ packages/esbuild-plugin-run/src/lib/esbuild-plugin-run.ts
@@ -1,7 +1,7 @@
-import execa, { Options } from 'execa';
+import execa, { Options, ExecaChildProcess } from 'execa';
 import path from 'path';
 import { Plugin } from 'esbuild';
-import chokidar from 'chokidar';
+import fs from 'fs-extra';
 
 const debug = require('debug')('plugin:run');
 
@@ -9,13 +9,37 @@ export interface RunOptions {
   execaOptions?: Options;
 }
 
+// 支持outDir，即多入口
+// 首次构建必定不存在，跳过
+//
+
 export default (options: RunOptions = {}): Plugin => {
+  let execaProcess: ExecaChildProcess<string>;
+
   return {
     name: 'esbuild:run',
     async setup({ initialOptions }) {
-      // if (!initialOptions.write) {
-      //   throw new Error('should write!');
-      // }
+      if (
+        initialOptions.write &&
+        typeof initialOptions.write === 'boolean' &&
+        (initialOptions.write as boolean) === false
+      ) {
+        throw new Error('should write!');
+      }
+
+      const fork = (filePath: string) => {
+        if (execaProcess && !execaProcess.killed) {
+          execaProcess.kill();
+        }
+
+        execaProcess = execa.node(filePath, {
+          stdio: 'inherit',
+          ...(options?.execaOptions ?? {}),
+        });
+
+        return execaProcess;
+      };
+
       debug('start');
 
       // support single entry only for now
@@ -25,17 +49,29 @@ export default (options: RunOptions = {}): Plugin => {
 
       const filePath = path.join(process.cwd(), initialOptions.outfile);
 
+      if (!fs.existsSync(filePath)) {
+        return;
+      }
+
+      process.stdin.resume();
+      process.stdin.setEncoding('utf8');
+
+      process.stdin.on('data', (data) => {
+        const line = data.toString().trim().toLowerCase();
+        console.log(data.toString().charCodeAt(0));
+        if (line === 'rs' || line === 'restart') {
+          fork(filePath);
+        } else if (line === 'cls' || line === 'clear') {
+          console.clear();
+        }
+      });
+
       return new Promise((resolve, reject) => {
         setTimeout(() => {
           debug('resolved');
-          execa
-            .node(filePath, {
-              stdio: 'inherit',
-              ...(options?.execaOptions ?? {}),
-            })
-            .then((cp) => {
-              resolve();
-            });
+          fork(filePath).then((cp) => {
+            resolve();
+          });
         }, 1000);
       });
     },
