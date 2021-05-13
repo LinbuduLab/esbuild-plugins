8df5754e098ad8a91e66b039365be6387db5eea3
linbudu599 <linbudu599@gmail.com>
2021-05-13 10:39:06 +0800

    :sparkles: feat(esbuild-run): initial implemention

--- packages/esbuild-plugin-run/src/lib/esbuild-plugin-run.spec.ts
+++
@@ -1,7 +0,0 @@
-import { esbuildPluginRun } from './esbuild-plugin-run';
-
-describe('esbuildPluginRun', () => {
-  it('should work', () => {
-    expect(esbuildPluginRun()).toEqual('esbuild-plugin-run');
-  });
-});
--- packages/esbuild-plugin-run/src/lib/esbuild-plugin-run.ts
+++ packages/esbuild-plugin-run/src/lib/esbuild-plugin-run.ts
@@ -1,3 +1,43 @@
-export function esbuildPluginRun(): string {
-  return 'esbuild-plugin-run';
+import execa, { Options } from 'execa';
+import path from 'path';
+import { Plugin } from 'esbuild';
+import chokidar from 'chokidar';
+
+const debug = require('debug')('plugin:run');
+
+export interface RunOptions {
+  execaOptions?: Options;
 }
+
+export default (options: RunOptions = {}): Plugin => {
+  return {
+    name: 'esbuild:run',
+    async setup({ initialOptions }) {
+      // if (!initialOptions.write) {
+      //   throw new Error('should write!');
+      // }
+      debug('start');
+
+      // support single entry only for now
+      if (!initialOptions.outfile) {
+        throw new Error('only single entry is supported!');
+      }
+
+      const filePath = path.join(process.cwd(), initialOptions.outfile);
+
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          debug('resolved');
+          execa
+            .node(filePath, {
+              stdio: 'inherit',
+              ...(options?.execaOptions ?? {}),
+            })
+            .then((cp) => {
+              resolve();
+            });
+        }, 1000);
+      });
+    },
+  };
+};
