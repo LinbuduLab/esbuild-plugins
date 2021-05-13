827b988f19644980454ce705807cb6e6d4bc476b
linbudu599 <linbudu599@gmail.com>
2021-05-13 22:27:44 +0800

    :sparkles: feat(nx-workspace): initial react-scripts integration

--- packages/nx-plugin-workspace/src/executors/react-scripts/executor.ts
+++ packages/nx-plugin-workspace/src/executors/react-scripts/executor.ts
@@ -1,8 +1,33 @@
 import { ReactScriptsExecutorSchema } from './schema';
+import { eachValueFrom } from 'rxjs-for-await';
+import { from, Observable } from 'rxjs';
+import { tap, map } from 'rxjs/operators';
+import execa from 'execa';
 
-export default async function runExecutor(options: ReactScriptsExecutorSchema) {
+// jest 版本问题
+// package.json missing
+// nx生成的app和react-scripts要求是不一致的
+// 这里要做一次文件检查
+// start 和 build会被整合到一起
+// 要做的预检查还蛮多的。。。
+
+const reactScriptsStart = (cwd: string) => {
+  return execa('react-scripts', ['start'], {
+    cwd,
+    preferLocal: true,
+    stdio: 'inherit',
+  });
+};
+
+export default function runExecutor(options: any) {
   console.log('Executor ran for ReactScripts', options);
-  return {
-    success: true,
-  };
+  return eachValueFrom(
+    from(reactScriptsStart(options.cwd)).pipe(
+      map(() => {
+        return {
+          success: true,
+        };
+      })
+    )
+  );
 }
