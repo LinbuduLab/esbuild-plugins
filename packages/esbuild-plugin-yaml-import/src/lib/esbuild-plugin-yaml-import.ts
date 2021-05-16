851c2d1cd7ad58c35e375152ad03c92f51444a97
linbudu599 <linbudu599@gmail.com>
2021-05-16 09:49:39 +0800

    :sparkles: feat(esbuild-yaml-import): initial implemention

--- packages/esbuild-plugin-yaml-import/src/lib/esbuild-plugin-yaml-import.ts
+++ packages/esbuild-plugin-yaml-import/src/lib/esbuild-plugin-yaml-import.ts
@@ -1,3 +1,51 @@
-export function esbuildPluginYamlImport(): string {
-  return 'esbuild-plugin-yaml-import';
+import path from 'path';
+import { Plugin } from 'esbuild';
+import fs from 'fs-extra';
+import chalk from 'chalk';
+import JSYaml, { LoadOptions as JSYamlOptions } from 'js-yaml';
+import { TextDecoder } from 'util';
+
+const debug = require('debug')('plugin:yaml');
+
+export interface Options {
+  jsyamlLoadOptions?: JSYamlOptions;
+  transformParsed?: (
+    data: string | number | object,
+    filePath: string
+  ) => object | undefined;
 }
+
+export default (options: Options = {}): Plugin => {
+  return {
+    name: 'esbuild:yaml',
+    setup({ onResolve, onLoad }) {
+      onResolve({ filter: /\.(yml|yaml)$/ }, (args) => {
+        if (args.resolveDir === '') return;
+
+        return {
+          path: path.isAbsolute(args.path)
+            ? args.path
+            : path.join(args.resolveDir, args.path),
+          namespace: 'yaml',
+        };
+      });
+
+      onLoad({ filter: /.*/, namespace: 'yaml' }, (args) => {
+        const yamlContent = fs.readFileSync(args.path);
+
+        let parsed = JSYaml.load(
+          new TextDecoder().decode(yamlContent),
+          options?.jsyamlLoadOptions ?? {}
+        );
+
+        if (options?.transformParsed)
+          parsed = options.transformParsed(parsed, args.path) ?? parsed;
+
+        return {
+          contents: JSON.stringify(parsed),
+          loader: 'json',
+        };
+      });
+    },
+  };
+};
