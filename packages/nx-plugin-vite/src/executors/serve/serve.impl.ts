20593bf57c99f553479a9b308f4402b283ccfbeb
Linbudu <48507806+linbudu599@users.noreply.github.com>
2021-10-24 12:22:34 +0800

    release(nx): 🔖 Release nx-plugin-vite@1.0.0

--- packages/nx-plugin-vite/src/executors/serve/serve.impl.ts
+++ packages/nx-plugin-vite/src/executors/serve/serve.impl.ts
@@ -6,10 +6,10 @@ import { eachValueFrom } from 'rxjs-for-await';
 import { catchError, map } from 'rxjs/operators';
 import consola from 'consola';
 
-import { startViteServer } from './lib/vite-serve';
+import { startViteAsync, startViteServer } from './lib/vite-serve';
 import { preflightCheck } from '../utils/preflight-check';
 
-export default function runExecutor(
+export default async function runExecutor(
   schema: ViteServeSchema,
   context: ExecutorContext
 ) {
@@ -18,15 +18,16 @@ export default function runExecutor(
   schema.root =
     schema.root ?? context.workspace.projects[context.projectName].root;
 
-  return eachValueFrom(
-    startViteServer(schema).pipe(
-      catchError((err, caught) => {
-        consola.error(err);
-        return of({
-          success: false,
-        });
-      }),
-      map((res) => res)
-    )
-  );
+  // return eachValueFrom(
+  //   startViteServer(schema).pipe(
+  //     catchError((err, caught) => {
+  //       consola.error(err);
+  //       return of({
+  //         success: false,
+  //       });
+  //     }),
+  //     map((res) => res)
+  //   )
+  // );
+  return await startViteAsync(schema);
 }
