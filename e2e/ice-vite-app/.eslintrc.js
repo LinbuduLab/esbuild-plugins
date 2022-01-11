0749aa083c13cecbc9be810628a7791566d46404
linbudu599 <linbudu599@gmail.com>
2022-01-11 19:13:21 +0800

    workspace(workspace): 🏗️ upgrade to nx 13, use split project config

--- e2e/ice-vite-app/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-const { getESLintConfig } = require('@iceworks/spec');
-
-module.exports = getESLintConfig('react-ts', {
-  rules: {
-    'react/jsx-filename-extension': 0,
-    '@typescript-eslint/explicit-function-return-type': 0,
-  },
-});
