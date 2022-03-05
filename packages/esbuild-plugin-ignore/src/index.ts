1b69b4f2a5695c08d2d1a6c6fe4b1bc3d09a0dd3
linbudu599 <linbudu599@gmail.com>
2022-03-05 16:35:16 +0800

    release(esbuild): 🏹 Release esbuild-plugin-ignore@0.5.1

--- packages/esbuild-plugin-ignore/src/index.ts
+++ packages/esbuild-plugin-ignore/src/index.ts
@@ -1,2 +1,10 @@
-import ignore from './lib/esbuild-plugin-ignore';
+import { ignore as _ignore } from './lib/esbuild-plugin-ignore';
+
+export const ignore = _ignore;
+
 export default ignore;
+
+export {
+  IgnorePattern,
+  ESBuildPluginIgnoreOption,
+} from './lib/esbuild-plugin-ignore';
